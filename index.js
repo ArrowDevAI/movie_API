5bf1d61d7ffb659e673f9e67d7adac90aa28d3bd
Trevor Higgins <higginst.th@gmail.com>
2024-07-17 14:02:01 -0700

    CORS put back

--- index.js
+++ index.js
@@ -23,7 +23,16 @@ app.use(express.static('public'));
 app.use(bodyParser.json());
 const cors = require ('cors');
 let allowedOrigins = ['http://127.0.0.1:8080', 'http://localhost:1234'];
-
+app.use(cors({
+    origin: (origin, callback)=>{
+        if (!origin) return callback (null, true);
+        if (allowedOrigins.indexOf(origin) === -1){
+            let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+            return callback (new Error(message), false);
+        }
+        return callback (null, true);
+    }
+}));
 let auth = require('./auth')(app);
 const passport = require('passport');
 require('./passport');
