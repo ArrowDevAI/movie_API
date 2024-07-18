037432eec2fd7cf43e4658c3f5d608fcb5002f26
Trevor Higgins <higginst.th@gmail.com>
2024-07-18 10:52:48 -0700

    reset cors

--- index.js
+++ index.js
@@ -24,7 +24,16 @@ const cors = require ('cors');
 let allowedOrigins = ['http://127.0.0.1:8080/', 'http://localhost:1234']
 
 
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback)=>{
+        if (!origin) return callback (null, true);
+        if (allowedOrigins.indexOf(origin) === -1){
+            let message = 'The CORS policy for this application does not allow access from origin ' + origin;
+            return callback (new Error(message), false);
+        }
+        return callback (null, true);
+    }
+}));
 
 let auth = require('./auth')(app);
 const passport = require('passport');
