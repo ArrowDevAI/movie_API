b8fd57219c20370837a2a3c7c79603ce7d0e857a
Trevor Higgins <higginst.th@gmail.com>
2024-07-31 11:46:57 -0700

    changed birthday type in users

--- models.js
+++ models.js
@@ -25,7 +25,7 @@ let userSchema = mongoose.Schema(
     Username: {type: String, required: true},
     Password: {type: String, required: true},
     Email: {type: String, required: true},
-    Birthday: Date,
+    Birthday: {type: Date},
     Img: {type: String},
     FavoriteMovies: [{type: mongoose.Schema.Types.ObjectID, ref: 'Movie'}]
 });
