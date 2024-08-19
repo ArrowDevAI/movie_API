f9d07420bcfaadebbb0ef8e4f38ecf8f1cfb7eb4
Trevor Higgins <higginst.th@gmail.com>
2024-08-19 07:58:49 -0400

    adjusted SK

--- auth.js
+++ auth.js
@@ -1,4 +1,4 @@
-const jwtSecret = 'your_jwt_secret'; // This has to be the same key used in the JWTStrategy
+const jwtSecret = 'buck_dancer'; // This has to be the same key used in the JWTStrategy
 
 const jwt = require('jsonwebtoken'),
   passport = require('passport');
@@ -9,7 +9,7 @@ require('./passport'); // Your local passport file
 let generateJWTToken = (user) => {
   return jwt.sign(user, jwtSecret, {
     subject: user.Username, // This is the username you’re encoding in the JWT
-    expiresIn: '1m', // This specifies that the token will expire in 1 Day
+    expiresIn: '1d', // This specifies that the token will expire in 1 Day
     algorithm: 'HS256' // This is the algorithm used to “sign” or encode the values of the JWT
   });
 }
